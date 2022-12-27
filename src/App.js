8b4c22f388cb0255911e20250c69159464a31612
Erick <estudodoerick@gmail.com>
2022-12-27 01:23:38 -0300

    array

--- src/App.js
+++ src/App.js
@@ -1,48 +1,49 @@
 import React from 'react';
 
-const luana = {
-  cliente: 'Luana',
-  idade: 27,
-  compras: [
-    { nome: 'Notebook', preco: 'R$2500' },
-    { nome: 'Geladeira', preco: 'R$3000' },
-    { nome: 'Smartphone', preco: 'R$1500' },
-  ],
-  ativa: true,
-};
-
-const mario = {
-  cliente: 'mario',
-  idade: 31,
-  compras: [
-    { nome: 'Notebook', preco: 'R$2500' },
-    { nome: 'Geladeira', preco: 'R$3000' },
-    { nome: 'Smartphone', preco: 'R$1500' },
-    { nome: 'Guitarra', preco: 'R$3500' },
-  ],
-  ativa: false,
-};
+const products = [
+  {
+    id: 1,
+    name: 'Smartphone',
+    value: 'R$2500',
+    color: ['#29d8d5', '#252a34', '#fc3766'],
+  },
+  {
+    id: 2,
+    name: 'Notebook',
+    value: 'R$3000',
+    color: ['#ffd045', '#d4394b', '#f37c59'],
+  },
+  {
+    id: 3,
+    name: 'Tablet',
+    value: 'R$1500',
+    color: ['#365069', '#47c1c8', '#f95786'],
+  },
+];
 
 const App = () => {
-  const dados = mario;
-
-  const totalGasto = dados.compras
-    .map((item) => Number(item.preco.replace('R$', '')))
-    .reduce((a, b) => a + b);
-
+  const data = products.filter(
+    ({ value }) => Number(value.replace('R$', '')) > 1500
+  );
   return (
-    <div>
-      <p>Nome: {dados.cliente}</p>
-      <p>Idade: {dados.idade}</p>
-      <p>
-        Situação:{' '}
-        <span style={{ color: dados.ativa ? 'green' : 'red' }}>
-          {dados.ativa ? 'Ativa' : 'Inativa'}
-        </span>
-      </p>
-      <p>Total gasto: {totalGasto}</p>
-      {totalGasto > 10000 && <p>Você está gastando muito, se controle!</p>}
-    </div>
+    <section>
+      {data.map(({ id, name, value, color }) => (
+        <div key={id}>
+          <h1>Produto: {name}</h1>
+          <p>Preço: {value}</p>
+          <ul>
+            {color.map((colors) => (
+              <li
+                style={{ backgroundColor: colors, color: 'white' }}
+                key={colors}
+              >
+                {colors}
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </section>
   );
 };
 
