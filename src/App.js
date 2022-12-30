e0b89d2a1203f520d0c5bde56ebe3887518fc8a6
Erick <estudodoerick@gmail.com>
2022-12-30 18:47:06 -0300

    props

--- src/App.js
+++ src/App.js
@@ -1,49 +1,16 @@
 import React from 'react';
 
-const products = [
-  {
-    id: 1,
-    name: 'Smartphone',
-    value: 'R$2500',
-    color: ['#29d8d5', '#252a34', '#fc3766'],
-  },
-  {
-    id: 2,
-    name: 'Notebook',
-    value: 'R$3000',
-    color: ['#ffd045', '#d4394b', '#f37c59'],
-  },
-  {
-    id: 3,
-    name: 'Tablet',
-    value: 'R$1500',
-    color: ['#365069', '#47c1c8', '#f95786'],
-  },
-];
+const Title = ({ color, text }) => {
+  return <h1 style={{ color: color }}>{text}</h1>;
+};
 
 const App = () => {
-  const data = products.filter(
-    ({ value }) => Number(value.replace('R$', '')) > 1500
-  );
   return (
-    <section>
-      {data.map(({ id, name, value, color }) => (
-        <div key={id}>
-          <h1>Produto: {name}</h1>
-          <p>Preço: {value}</p>
-          <ul>
-            {color.map((colors) => (
-              <li
-                style={{ backgroundColor: colors, color: 'white' }}
-                key={colors}
-              >
-                {colors}
-              </li>
-            ))}
-          </ul>
-        </div>
-      ))}
-    </section>
+    <div>
+      <Title color="red" text="Meu titulo 1" />
+      <Title color="blue" text="Meu titulo 2" />
+      <Title color="green" text="Meu titulo 3" />
+    </div>
   );
 };
 
