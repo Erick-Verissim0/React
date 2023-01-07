a962934193a02215caa1e4035ef6b043c130ecd9
Erick <estudodoerick@gmail.com>
2023-01-07 18:49:33 -0300

    React.useRef_and_current

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import React from 'react';
 
 const App = () => {
-  const [count, setCount] = React.useState(1);
-  const [items, setItems] = React.useState(['Item 1']);
+  const video = React.useRef();
 
-  function handleClick() {
-    setCount((count) => count + 1); // poderia fazer como está na linha abaixo
-    setItems([...items, 'Item' + (count + 1)]); // não estou passando a const como parametro do uma arrow function porque ja estou usando a própria constante dentro
-  }
+  React.useEffect(() => {
+    video.current = 'test';
+    console.log(video.current);
+  }, []);
 
-  return (
-    <div>
-      {items.map((item) => (
-        <li key={item}>{item}</li>
-      ))}
-      <button onClick={handleClick}>Click</button>
-    </div>
-  );
+  return <video ref={video} />;
 };
 
 export default App;
