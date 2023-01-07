fff006d71ee5bed7524564059328fe489ddc91bc
Erick <estudodoerick@gmail.com>
2023-01-07 19:37:43 -0300

    useRef_1.2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import React from 'react';
 
 const App = () => {
-  const video = React.useRef();
+  const [coments, setComents] = React.useState([]);
+  const [input, setInput] = React.useState('');
+  const inputElement = React.useRef();
 
-  React.useEffect(() => {
-    video.current = 'test';
-    console.log(video.current);
-  }, []);
+  function handleClick() {
+    setComents([...coments, input]);
+    setInput('');
+    inputElement.current.focus();
+  }
 
-  return <video ref={video} />;
+  return (
+    <div>
+      <ul>
+        {coments.map((coments) => (
+          <li key={coments}>{coments}</li>
+        ))}
+      </ul>
+      <input
+        type="text"
+        ref={inputElement}
+        value={input}
+        onChange={({ target }) => setInput(target.value)}
+      />
+      <button onClick={handleClick}>Send</button>
+    </div>
+  );
 };
 
 export default App;
